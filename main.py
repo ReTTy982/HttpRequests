7a7e91c01d7145127734cf4506e8ce7bf63aceaf
ReTTy982 <polekpacz@gmail.com>
2023-02-01 16:04:41 +0100

    123

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import json
 import datetime
 from datetime import timedelta
 
+local = "http://127.0.0.1:8000/wypozyczalnia/"
+server = "http://70.34.244.229:8081/wypozyczalnia/"
+url_global = server
+
 def register():
     url = 'http://70.34.244.229:8081/wypozyczalnia/my_register'
 
@@ -24,17 +28,17 @@ def search_book():
     print(r.content)
 
 def login():
-    url="http://127.0.0.1:8000/wypozyczalnia/my_login"
+    url= url_global + "my_login"
     #url = "http://127.0.0.1:8000/wypozyczalnia/test"
     session = requests.Session()
-    password = "zdutton0"
-    username = "uKnMDFnLyMkt"
+    password = "orMHO4gp8dq"
+    username = "jelgee0"
     session.auth = (username, password)
     auth = session.post("http://127.0.0.1:8000/wypozyczalnia/my_login")
     response = session.get("http://127.0.0.1:8000/wypozyczalnia/my_login")
     #response = requests.post(url=url, auth=(username, password))
     #x = response.content.decode("utf-8")
-    return session
+    return auth
 
 def search_my_books(x):
     
@@ -61,30 +65,47 @@ def serial_add():
         print(json.dumps(parsed,indent=4))
     else:
         print(request.status_code)
+        print(request.content)
 
 def book_copy_get():
-    url = url = "http://70.34.244.229:8081/wypozyczalnia/book_copies"
-    data = {'book_id':5,'branch_id':8}
-    data = {'book_id':1}
+    url = url_global + "book_copies"
+    data = {'id': 1}
+    #data = {'book_id': 1}
     #data = {}
-    request = requests.get(url=url,data=data)
+    #data =None
+    request = requests.get(url=url,params=data)
+    print(request.url)
     if request.status_code != 400:
+        print(request.status_code)
         parsed = json.loads(request.content)
         print(json.dumps(parsed,indent=4))
     else:
         print(request.status_code)
+        print(request.content)
+
+    print(request.url)
+def book_copy_add():
+    url = url_global + "book_copies"
+    data = {'id' : 25, 'delete':1}
+    request = requests.post(url, data=data)
+    parsed = json.loads(request.content)
+    print(json.dumps(parsed,indent=4))
+
 
 def book_add():
-    url = url = "http://127.0.0.1:8000/wypozyczalnia/books"
-    data = {'author':6,'publisher_name':'Mohr Group', 'category' : 'Drama','book_title':'testowanie'}
+    url = url_global +"books"
+    data = {'author':6,'publisher_name':'Mohr Group', 'category' : 'Drama','book_title':'testowniś'}
     request = requests.post(url=url,data=data)
     parsed = json.loads(request.content)
     print(json.dumps(parsed,indent=4))
 
+def book_get():
+    url = url_global +"books"
+
 def get_authors():
-    url = url = "http://127.0.0.1:8000/wypozyczalnia/authors"
-    data = {'author_name' : "Ad Spurgeon"}
-    request = requests.get(url=url,data=data)
+    url = url_global + "authors"
+    data = {'author_name' : "Gloria Burdass"}
+    request = requests.get(url=url,params=data)
     if request.status_code != 400:
         parsed = json.loads(request.content)
         print(json.dumps(parsed,indent=4))
@@ -93,21 +114,31 @@ def get_authors():
 
 def add_authors():
     url = url = "http://127.0.0.1:8000/wypozyczalnia/authors"
-    data = {'author_name' : "Ad KAKAKAKKA"}
+    data = {'author_name' : "Gloria Burdass"}
+    data = {'author_name' : '1'}
     request = requests.post(url=url,data=data)
+    if request.status_code != 400:
+        parsed = json.loads(request.content)
+        print(json.dumps(parsed,indent=4))
+    else:
+        print(request.content)
+    
 
 def add_issue():
     url = url = "http://127.0.0.1:8000/wypozyczalnia/issue_book"
     date_issue = datetime.datetime.now()
+    print(date_issue)
+    print(type(date_issue))
     date_due = datetime.datetime.now() + datetime.timedelta(20)
     data = {'copy': 10,'library_user':1,'date_issue' :date_issue, 'date_due' :date_due,'operation':'oddaj'}
     data = {'operation': 'oddaj', 'id': 5005}
+    data = {'copy': 10,'library_user':1,'date_issue' :date_issue, 'date_due' :date_due,'operation':'wypozycz'}
     request = requests.post(url=url,data=data)
 
 def get_issue():
     url ="http://127.0.0.1:8000/wypozyczalnia/issue_book"
-    data = {'user_id' : 48}
-    request = requests.get(url=url,data=data)
+    data = {'user_id' : 1}
+    request = requests.get(url=url,params=data)
     if request.status_code != 400:
         parsed = json.loads(request.content)
         print(json.dumps(parsed,indent=4))
@@ -115,7 +146,7 @@ def get_issue():
         print(request.status_code)
 
 def get_user():
-    url ="http://127.0.0.1:8000/wypozyczalnia/library_user"
+    url ="http://70.34.244.229:8081/wypozyczalnia/library_user"
     data = {'first_name' : 'Gregoire'}
     request = requests.get(url, data=data)
     if request.status_code != 400:
@@ -128,5 +159,19 @@ def add_user():
     url ="http://127.0.0.1:8000/wypozyczalnia/library_user"
     data = {'first_name' : 'Polek','last_name' : 'Paczynski','login' : "poleczek",'password': 'krewetka1998$','address' : "Nie wiem"}
     request = requests.post(url, data=data)
+def logout(session):
+    url = local + "logout"
+    requests.get(url=url)
+    
 if __name__ == "__main__":
     book_copy_get()
+    #book_copy_add()
+    #book_copy_get()
+    #book_add()
+    # get_authors()
+     #add_authors()
+    # add_issue()
+    # get_issue()
+    # get_user()
+    # add_user()
+    
